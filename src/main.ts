f94037cd8295f1fb708e98187dca3c47e01a6d2d
Siraj Gadhia <Siraj360@users.noreply.github.com>
2025-11-15 14:19:35 -0500

    Refactor bootstrapApplication with router and location strategy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideLocationStrategy, HashLocationStrategy } from '@angular/common';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    provideRouter(routes),
+    provideLocationStrategy(HashLocationStrategy)
+  ]
+})
+.catch((err) => console.error(err));
